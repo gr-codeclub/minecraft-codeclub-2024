77c5126e7409b64fc797928a25eca7820ea52094
codeclub20231 <134066375+codeclub20231@users.noreply.github.com>
2025-04-03 16:57:55 +0100

    i made my skibidi block into a house {unfinished}

--- jacobmod/src/main/java/net/mcreator/dextermod/procedures/SkibidiblockOnBlockRightClickedProcedure.java
+++ jacobmod/src/main/java/net/mcreator/dextermod/procedures/SkibidiblockOnBlockRightClickedProcedure.java
@@ -15,11 +15,33 @@ public class SkibidiblockOnBlockRightClickedProcedure {
 		for (int index0 = 0; index0 < 5; index0++) {
 			for (int index1 = 0; index1 < 5; index1++) {
 				world.setBlock(BlockPos.containing(x + xoffset, y - 1, z + zoffset), Blocks.MAGMA_BLOCK.defaultBlockState(), 3);
-				world.setBlock(BlockPos.containing(x + xoffset, y - 3, z + zoffset), Blocks.MAGMA_BLOCK.defaultBlockState(), 3);
+				world.setBlock(BlockPos.containing(x + xoffset, y + 3, z + zoffset), Blocks.MAGMA_BLOCK.defaultBlockState(), 3);
 				xoffset = xoffset + 1;
 			}
 			xoffset = -2;
 			zoffset = zoffset + 1;
+		}
+		xoffset = -2;
+		yoffset = 0;
+		for (int index2 = 0; index2 < 3; index2++) {
+			for (int index3 = 0; index3 < 5; index3++) {
+				world.setBlock(BlockPos.containing(x + xoffset, y + yoffset, z + 2), Blocks.MAGMA_BLOCK.defaultBlockState(), 3);
+				world.setBlock(BlockPos.containing(x + xoffset, y + yoffset, z - 2), Blocks.MAGMA_BLOCK.defaultBlockState(), 3);
+				xoffset = xoffset + 1;
+			}
+			xoffset = -2;
+			yoffset = yoffset + 1;
+		}
+		yoffset = 0;
+		zoffset = -2;
+		for (int index4 = 0; index4 < 3; index4++) {
+			for (int index5 = 0; index5 < 5; index5++) {
+				world.setBlock(BlockPos.containing(x + 2, y + yoffset, z + zoffset), Blocks.MAGMA_BLOCK.defaultBlockState(), 3);
+				world.setBlock(BlockPos.containing(x - 2, y + yoffset, z + zoffset), Blocks.MAGMA_BLOCK.defaultBlockState(), 3);
+				zoffset = zoffset + 1;
+			}
+			xoffset = -2;
+			yoffset = yoffset + 1;
 		}
 	}
 }
