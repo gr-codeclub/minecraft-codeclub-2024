f15ffee7ad76e686ee21f9a28c482fde48ce46e5
codeclub202310 <codeclub202310@outlook.com>
2025-04-03 17:01:47 +0100

    cerrfhdtdr

--- yousefmod/src/main/java/net/mcreator/yousefmod/block/FootballBlock.java
+++ yousefmod/src/main/java/net/mcreator/yousefmod/block/FootballBlock.java
@@ -2,27 +2,46 @@
 package net.mcreator.yousefmod.block;
 
 import net.minecraft.world.phys.BlockHitResult;
+import net.minecraft.world.level.material.PushReaction;
+import net.minecraft.world.level.block.state.properties.EnumProperty;
+import net.minecraft.world.level.block.state.StateDefinition;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.level.block.state.BlockBehaviour;
 import net.minecraft.world.level.block.SoundType;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.BlockGetter;
+import net.minecraft.world.item.context.BlockPlaceContext;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.InteractionResult;
+import net.minecraft.util.StringRepresentable;
 import net.minecraft.core.Direction;
 import net.minecraft.core.BlockPos;
 
 import net.mcreator.yousefmod.procedures.HousefbuilderProcedure;
 
 public class FootballBlock extends Block {
+	public static final EnumProperty<FProperty> F = EnumProperty.create("f", FProperty.class);
+
 	public FootballBlock() {
-		super(BlockBehaviour.Properties.of().sound(SoundType.GRAVEL).strength(1f, 10f));
+		super(BlockBehaviour.Properties.of().sound(SoundType.GRAVEL).strength(1f, 10f).pushReaction(PushReaction.IGNORE));
+		this.registerDefaultState(this.stateDefinition.any().setValue(F, FProperty.ROOF));
 	}
 
 	@Override
 	public int getLightBlock(BlockState state, BlockGetter worldIn, BlockPos pos) {
 		return 15;
+	}
+
+	@Override
+	protected void createBlockStateDefinition(StateDefinition.Builder<Block, BlockState> builder) {
+		super.createBlockStateDefinition(builder);
+		builder.add(F);
+	}
+
+	@Override
+	public BlockState getStateForPlacement(BlockPlaceContext context) {
+		return super.getStateForPlacement(context).setValue(F, FProperty.ROOF);
 	}
 
 	@Override
@@ -37,5 +56,20 @@ public class FootballBlock extends Block {
 		Direction direction = hit.getDirection();
 		HousefbuilderProcedure.execute(world, x, y, z);
 		return InteractionResult.SUCCESS;
+	}
+
+	public enum FProperty implements StringRepresentable {
+		ROOF("roof"), WALL("wall"), FLOOR("floor");
+
+		private final String name;
+
+		private FProperty(String name) {
+			this.name = name;
+		}
+
+		@Override
+		public String getSerializedName() {
+			return this.name;
+		}
 	}
 }
--- yousefmod/src/main/java/net/mcreator/yousefmod/procedures/HousefbuilderProcedure.java
+++ yousefmod/src/main/java/net/mcreator/yousefmod/procedures/HousefbuilderProcedure.java
@@ -1,8 +1,5 @@
 package net.mcreator.yousefmod.procedures;
 
-import net.minecraft.world.level.block.state.properties.Property;
-import net.minecraft.world.level.block.state.properties.EnumProperty;
-import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.level.block.Blocks;
 import net.minecraft.world.level.LevelAccessor;
 import net.minecraft.core.BlockPos;
@@ -12,46 +9,40 @@ public class HousefbuilderProcedure {
 		double zOffset = 0;
 		double xOffset = 0;
 		double yOffset = 0;
-		zOffset = -1;
-		xOffset = -1;
-		yOffset = -1;
-		for (int index0 = 0; index0 < 3; index0++) {
-			for (int index1 = 0; index1 < 3; index1++) {
-				world.setBlock(BlockPos.containing(x + xOffset, y - 1, z + zOffset), Blocks.STRIPPED_BIRCH_WOOD.defaultBlockState(), 3);
+		zOffset = -2;
+		xOffset = -2;
+		yOffset = 0;
+		for (int index0 = 0; index0 < 5; index0++) {
+			for (int index1 = 0; index1 < 5; index1++) {
+				world.setBlock(BlockPos.containing(x + xOffset, y - 1, z + zOffset), Blocks.SMOOTH_SANDSTONE.defaultBlockState(), 3);
+				world.setBlock(BlockPos.containing(x + xOffset, y + 3, z + zOffset), Blocks.SMOOTH_SANDSTONE.defaultBlockState(), 3);
 				xOffset = 1 + xOffset;
 			}
-			xOffset = -1;
+			xOffset = -2;
 			zOffset = 1 + zOffset;
 		}
-		zOffset = -1;
-		xOffset = -1;
-		yOffset = -1;
+		xOffset = -2;
+		yOffset = 0;
 		for (int index2 = 0; index2 < 3; index2++) {
-			for (int index3 = 0; index3 < 3; index3++) {
-				world.setBlock(BlockPos.containing(x + xOffset, y + yOffset, z + zOffset), Blocks.STRIPPED_BIRCH_WOOD.defaultBlockState(), 3);
+			for (int index3 = 0; index3 < 5; index3++) {
+				world.setBlock(BlockPos.containing(x + xOffset, y + yOffset, z + 2), Blocks.SMOOTH_SANDSTONE.defaultBlockState(), 3);
+				world.setBlock(BlockPos.containing(x + xOffset, y + yOffset, z - 2), Blocks.SMOOTH_SANDSTONE.defaultBlockState(), 3);
 				xOffset = 1 + xOffset;
 			}
-			xOffset = -1;
+			xOffset = -2;
 			yOffset = 1 + yOffset;
 		}
+		zOffset = -2;
+		xOffset = -2;
+		yOffset = 0;
 		for (int index4 = 0; index4 < 3; index4++) {
-			for (int index5 = 0; index5 < 3; index5++) {
-				world.setBlock(BlockPos.containing(x + xOffset, y + 10, z + zOffset), Blocks.STRIPPED_BIRCH_WOOD.defaultBlockState(), 3);
-				xOffset = 1 + xOffset;
+			for (int index5 = 0; index5 < 5; index5++) {
+				world.setBlock(BlockPos.containing(x + 2, y + yOffset, z + zOffset), Blocks.SMOOTH_SANDSTONE.defaultBlockState(), 3);
+				world.setBlock(BlockPos.containing(x - 2, y + yOffset, z + zOffset), Blocks.SMOOTH_SANDSTONE.defaultBlockState(), 3);
+				zOffset = 1 + zOffset;
 			}
-			xOffset = -1;
-			zOffset = 1 + zOffset;
+			zOffset = -2;
+			yOffset = 1 + yOffset;
 		}
-		world.setBlock(BlockPos.containing(x + 1, y, z + 2), Blocks.GLASS.defaultBlockState(), 3);
-		world.setBlock(BlockPos.containing(x + 1, y, z + 2), Blocks.GLASS.defaultBlockState(), 3);
-		world.setBlock(BlockPos.containing(x - 1, y, z + 2), Blocks.AIR.defaultBlockState(), 3);
-		world.setBlock(BlockPos.containing(x - 1, y + 1, z + 2), Blocks.AIR.defaultBlockState(), 3);
-		world.setBlock(BlockPos.containing(x - 1, y, z + 2), Blocks.SPRUCE_DOOR.defaultBlockState(), 3);
-		world.setBlock(BlockPos.containing(x - 1, y, z + 2), (new Object() {
-			public BlockState with(BlockState _bs, String _property, String _newValue) {
-				Property<?> _prop = _bs.getBlock().getStateDefinition().getProperty(_property);
-				return _prop instanceof EnumProperty _ep && _ep.getValue(_newValue).isPresent() ? _bs.setValue(_ep, (Enum) _ep.getValue(_newValue).get()) : _bs;
-			}
-		}.with(Blocks.SPRUCE_DOOR.defaultBlockState(), "half", "upper")), 3);
 	}
 }
