3a5ecd158a5688e2a691eada6ce399d628ba5fad
codeclub20236 <134069199+codeclub20236@users.noreply.github.com>
2025-05-08 17:02:04 +0100

    i sat on a chair

--- erikmod/src/main/java/net/mcreator/erikmod/entity/ErikblueEntity.java
+++ erikmod/src/main/java/net/mcreator/erikmod/entity/ErikblueEntity.java
@@ -4,12 +4,15 @@ package net.mcreator.erikmod.entity;
 import net.neoforged.neoforge.event.entity.RegisterSpawnPlacementsEvent;
 
 import net.minecraft.world.phys.Vec3;
+import net.minecraft.world.level.ServerLevelAccessor;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.entity.ai.attributes.Attributes;
 import net.minecraft.world.entity.ai.attributes.AttributeSupplier;
+import net.minecraft.world.entity.SpawnGroupData;
 import net.minecraft.world.entity.PathfinderMob;
+import net.minecraft.world.entity.MobSpawnType;
 import net.minecraft.world.entity.Mob;
 import net.minecraft.world.entity.EntityType;
 import net.minecraft.world.entity.EntityDimensions;
@@ -17,12 +20,16 @@ import net.minecraft.world.entity.Entity;
 import net.minecraft.world.damagesource.DamageSource;
 import net.minecraft.world.InteractionResult;
 import net.minecraft.world.InteractionHand;
+import net.minecraft.world.DifficultyInstance;
 import net.minecraft.sounds.SoundEvent;
 import net.minecraft.server.level.ServerLevel;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.core.registries.BuiltInRegistries;
 
+import net.mcreator.erikmod.procedures.ErikblueOnInitialEntitySpawnProcedure;
 import net.mcreator.erikmod.init.ErikmodModItems;
+
+import javax.annotation.Nullable;
 
 public class ErikblueEntity extends PathfinderMob {
 	public ErikblueEntity(EntityType<ErikblueEntity> type, Level world) {
@@ -55,6 +62,13 @@ public class ErikblueEntity extends PathfinderMob {
 	@Override
 	public SoundEvent getDeathSound() {
 		return BuiltInRegistries.SOUND_EVENT.get(ResourceLocation.parse("entity.generic.death"));
+	}
+
+	@Override
+	public SpawnGroupData finalizeSpawn(ServerLevelAccessor world, DifficultyInstance difficulty, MobSpawnType reason, @Nullable SpawnGroupData livingdata) {
+		SpawnGroupData retval = super.finalizeSpawn(world, difficulty, reason, livingdata);
+		ErikblueOnInitialEntitySpawnProcedure.execute(world, this.getX(), this.getY(), this.getZ(), this);
+		return retval;
 	}
 
 	@Override
