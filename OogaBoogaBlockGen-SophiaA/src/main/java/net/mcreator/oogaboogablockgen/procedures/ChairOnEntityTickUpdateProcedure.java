065991c72a610d3a0e75822085a8c9422ed1109e
codeclub202311 <134087385+codeclub202311@users.noreply.github.com>
2025-05-15 17:04:19 +0100

    ryyhtngsgrnh,

---
+++ OogaBoogaBlockGen-SophiaA/src/main/java/net/mcreator/oogaboogablockgen/procedures/ChairOnEntityTickUpdateProcedure.java
@@ -0,0 +1,29 @@
+package net.mcreator.oogaboogablockgen.procedures;
+
+import net.minecraft.world.phys.Vec3;
+import net.minecraft.world.level.LevelAccessor;
+import net.minecraft.world.entity.Entity;
+import net.minecraft.util.RandomSource;
+import net.minecraft.util.Mth;
+import net.minecraft.core.particles.ParticleTypes;
+
+import java.util.ArrayList;
+
+public class ChairOnEntityTickUpdateProcedure {
+	public static void execute(LevelAccessor world, double x, double y, double z, Entity entity) {
+		if (entity == null)
+			return;
+		if (entity.isVehicle()) {
+			world.addParticle(ParticleTypes.FIREWORK, x, y, z, 1, 1, 1);
+			world.addParticle(ParticleTypes.FIREWORK, x, y, z, (-1), 1, (-1));
+			world.addParticle(ParticleTypes.FIREWORK, x, y, z, 0, 1, 0);
+			world.addParticle(ParticleTypes.FIREWORK, x, y, z, 0, 1, 0);
+			if (Mth.nextInt(RandomSource.create(), 1, 100) > 99) {
+				for (Entity entityiterator : new ArrayList<>(entity.getPassengers())) {
+					entity.stopRiding();
+					entity.setDeltaMovement(new Vec3(0, 10, 0));
+				}
+			}
+		}
+	}
+}
