18ea36c53675bf70f0eb0525e3c68c11a924a06e
codeclub20234 <134068441+codeclub20234@users.noreply.github.com>
2025-05-15 16:56:35 +0100

    i flew with a chair

--- erikmod/src/main/java/net/mcreator/erikmod/entity/ErikblueEntity.java
+++ erikmod/src/main/java/net/mcreator/erikmod/entity/ErikblueEntity.java
@@ -27,6 +27,7 @@ import net.minecraft.resources.ResourceLocation;
 import net.minecraft.core.registries.BuiltInRegistries;
 
 import net.mcreator.erikmod.procedures.ErikblueOnInitialEntitySpawnProcedure;
+import net.mcreator.erikmod.procedures.ErikblueOnEntityTickUpdateProcedure;
 import net.mcreator.erikmod.init.ErikmodModItems;
 
 import javax.annotation.Nullable;
@@ -78,6 +79,12 @@ public class ErikblueEntity extends PathfinderMob {
 		super.mobInteract(sourceentity, hand);
 		sourceentity.startRiding(this);
 		return retval;
+	}
+
+	@Override
+	public void baseTick() {
+		super.baseTick();
+		ErikblueOnEntityTickUpdateProcedure.execute(this.level(), this.getX(), this.getY(), this.getZ(), this);
 	}
 
 	public static void init(RegisterSpawnPlacementsEvent event) {
---
+++ erikmod/src/main/java/net/mcreator/erikmod/procedures/ErikblueOnEntityTickUpdateProcedure.java
@@ -0,0 +1,29 @@
+package net.mcreator.erikmod.procedures;
+
+import net.minecraft.world.phys.Vec3;
+import net.minecraft.world.level.LevelAccessor;
+import net.minecraft.world.entity.Entity;
+import net.minecraft.util.RandomSource;
+import net.minecraft.util.Mth;
+import net.minecraft.core.particles.ParticleTypes;
+
+import java.util.ArrayList;
+
+public class ErikblueOnEntityTickUpdateProcedure {
+	public static void execute(LevelAccessor world, double x, double y, double z, Entity entity) {
+		if (entity == null)
+			return;
+		if (entity.isVehicle()) {
+			world.addParticle(ParticleTypes.DRAGON_BREATH, x, y, z, 1, 1, 1);
+			world.addParticle(ParticleTypes.DRAGON_BREATH, x, y, z, (-1), 1, (-1));
+			world.addParticle(ParticleTypes.DRAGON_BREATH, x, y, z, 0, 1, 0);
+			world.addParticle(ParticleTypes.DRAGON_BREATH, x, y, z, 0, 1, 0);
+			if (Mth.nextInt(RandomSource.create(), 1, 100) >= 99) {
+				for (Entity entityiterator : new ArrayList<>(entity.getPassengers())) {
+					entityiterator.stopRiding();
+					entityiterator.setDeltaMovement(new Vec3(0, 10, 0));
+				}
+			}
+		}
+	}
+}
