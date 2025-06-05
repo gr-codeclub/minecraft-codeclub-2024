f21f2aa3ec2fbfe6eea900704d5fdbc066190d37
codeclub20233 <codeclub20233@outlook.com>
2025-06-05 16:56:02 +0100

    56rtx5t43trgdefbhgrfg

--- OogaBoogaBlockGen-SophiaA/src/main/java/net/mcreator/oogaboogablockgen/item/BananaItem.java
+++ OogaBoogaBlockGen-SophiaA/src/main/java/net/mcreator/oogaboogablockgen/item/BananaItem.java
@@ -2,40 +2,26 @@
 package net.mcreator.oogaboogablockgen.item;
 
 import net.minecraft.world.level.block.state.BlockState;
-import net.minecraft.world.level.Level;
 import net.minecraft.world.item.component.ItemAttributeModifiers;
-import net.minecraft.world.item.UseAnim;
 import net.minecraft.world.item.Rarity;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.Item;
-import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.food.FoodProperties;
 import net.minecraft.world.entity.ai.attributes.Attributes;
 import net.minecraft.world.entity.ai.attributes.AttributeModifier;
 import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.EquipmentSlotGroup;
-import net.minecraft.world.InteractionResultHolder;
-import net.minecraft.world.InteractionHand;
 
 public class BananaItem extends Item {
 	public BananaItem() {
-		super(new Item.Properties().durability(64).fireResistant().rarity(Rarity.EPIC)
+		super(new Item.Properties().durability(64).fireResistant().rarity(Rarity.EPIC).food((new FoodProperties.Builder()).nutrition(1000).saturationModifier(1000f).alwaysEdible().build())
 				.attributes(ItemAttributeModifiers.builder().add(Attributes.ATTACK_DAMAGE, new AttributeModifier(BASE_ATTACK_DAMAGE_ID, 73, AttributeModifier.Operation.ADD_VALUE), EquipmentSlotGroup.MAINHAND)
 						.add(Attributes.ATTACK_SPEED, new AttributeModifier(BASE_ATTACK_SPEED_ID, -2.4, AttributeModifier.Operation.ADD_VALUE), EquipmentSlotGroup.MAINHAND).build()));
 	}
 
 	@Override
-	public UseAnim getUseAnimation(ItemStack itemstack) {
-		return UseAnim.EAT;
-	}
-
-	@Override
 	public int getEnchantmentValue() {
 		return 2736;
-	}
-
-	@Override
-	public int getUseDuration(ItemStack itemstack, LivingEntity livingEntity) {
-		return 32;
 	}
 
 	@Override
@@ -46,13 +32,6 @@ public class BananaItem extends Item {
 	@Override
 	public boolean isCorrectToolForDrops(ItemStack itemstack, BlockState state) {
 		return true;
-	}
-
-	@Override
-	public InteractionResultHolder<ItemStack> use(Level world, Player entity, InteractionHand hand) {
-		InteractionResultHolder<ItemStack> ar = super.use(world, entity, hand);
-		entity.startUsingItem(hand);
-		return ar;
 	}
 
 	@Override
