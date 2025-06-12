52ab48a2fc37ebdcb74ae8bf5e85b9545751e385
codeclub20231 <134066375+codeclub20231@users.noreply.github.com>
2025-06-12 17:00:59 +0100

    save

---
+++ wingsoffire/models/mojmap-1.21.x/Modelscott.java
@@ -0,0 +1,53 @@
+// Made with Blockbench 4.8.1
+// Exported for Minecraft version 1.17 or later with Mojang mappings
+// Paste this class into your mod and generate all required imports
+
+public class Modelscott<T extends Entity> extends EntityModel<T> {
+	// This layer location should be baked with EntityRendererProvider.Context in
+	// the entity renderer and passed into this model's constructor
+	public static final ModelLayerLocation LAYER_LOCATION = new ModelLayerLocation(
+			new ResourceLocation("modid", "scott"), "main");
+	private final ModelPart bb_main;
+
+	public Modelscott(ModelPart root) {
+		this.bb_main = root.getChild("bb_main");
+	}
+
+	public static LayerDefinition createBodyLayer() {
+		MeshDefinition meshdefinition = new MeshDefinition();
+		PartDefinition partdefinition = meshdefinition.getRoot();
+
+		PartDefinition bb_main = partdefinition.addOrReplaceChild("bb_main",
+				CubeListBuilder.create().texOffs(0, 0)
+						.addBox(-7.0F, -15.0F, -8.0F, 16.0F, 16.0F, 16.0F, new CubeDeformation(-1.0F)).texOffs(-14, -14)
+						.addBox(-7.0F, -15.0F, -7.0F, 16.0F, 2.0F, 16.0F, new CubeDeformation(0.0F)).texOffs(1, 1)
+						.addBox(-2.0F, -15.0F, 9.0F, 6.0F, 1.0F, 1.0F, new CubeDeformation(0.0F)).texOffs(1, 1)
+						.addBox(-2.0F, -16.0F, 10.0F, 6.0F, 1.0F, 1.0F, new CubeDeformation(0.0F)).texOffs(1, 1)
+						.addBox(-2.0F, -17.0F, 11.0F, 6.0F, 1.0F, 1.0F, new CubeDeformation(0.0F)).texOffs(1, 1)
+						.addBox(-2.0F, -18.0F, 12.0F, 6.0F, 1.0F, 1.0F, new CubeDeformation(0.0F)).texOffs(0, 0)
+						.addBox(-7.0F, -15.0F, -7.0F, 6.0F, 2.0F, 2.0F, new CubeDeformation(0.0F)).texOffs(0, 0)
+						.addBox(4.0F, -10.0F, 6.0F, 1.0F, 1.0F, 2.0F, new CubeDeformation(0.0F)).texOffs(0, 0)
+						.addBox(-3.0F, -10.0F, 6.0F, 1.0F, 1.0F, 2.0F, new CubeDeformation(0.0F)).texOffs(0, 0)
+						.addBox(-2.0F, -7.0F, 6.0F, 6.0F, 1.0F, 2.0F, new CubeDeformation(0.0F)),
+				PartPose.offset(0.0F, 24.0F, 0.0F));
+
+		PartDefinition cube_r1 = bb_main.addOrReplaceChild("cube_r1",
+				CubeListBuilder.create().texOffs(0, 0).addBox(8.0F, 1.2567F, 16.2846F, 6.0F, 1.0F, 2.0F,
+						new CubeDeformation(0.0F)),
+				PartPose.offsetAndRotation(0.0F, 0.0F, -16.0F, -0.0873F, 0.0F, 0.0F));
+
+		return LayerDefinition.create(meshdefinition, 64, 64);
+	}
+
+	@Override
+	public void setupAnim(T entity, float limbSwing, float limbSwingAmount, float ageInTicks, float netHeadYaw,
+			float headPitch) {
+
+	}
+
+	@Override
+	public void renderToBuffer(PoseStack poseStack, VertexConsumer vertexConsumer, int packedLight, int packedOverlay,
+			float red, float green, float blue, float alpha) {
+		bb_main.render(poseStack, vertexConsumer, packedLight, packedOverlay, red, green, blue, alpha);
+	}
+}
